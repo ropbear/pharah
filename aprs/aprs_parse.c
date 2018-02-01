c26b04e7494277edc027a0ba2afcf338371bb486
stonepresto <prestonpdppritchard@gmail.com>
2018-02-01 11:56:55 -0500

    working dynamic

--- aprs/aprs_parse.c
+++ aprs/aprs_parse.c
@@ -26,63 +26,11 @@ https://www.tapr.org/pdf/AX25.2.2.pdf
 
 void error_handle (char * error_string);
 void print_packet (char * dest,char * src, char * dgpt, char * data);
+int recieve(void);
 
 int main (int argc, char * argv[]) 
 {
-	unsigned int c,findex;
-	unsigned long throughput;
-	int recv,addr,mid,info;
-	char dest[ADDR_MAX];
-	char src[ADDR_MAX];
-	char dgpt[DGPT_MAX];
-	char data[DATA_MAX];
-
-	findex = throughput = 0;
-	addr = 1;
-	recv = mid = info = 0;
-
-	while ((c = getchar()) != EOF) {
-		if (1 == recv) {
-			if (1 == addr) {
-				if (c == CTLFLAG)
-					mid = 1;
-				else if (c == PIDFLAG && mid == 1) {
-					addr = 0;
-					info = 1;
-					findex = 0;
-				}
-				else if (findex < DGPT_MAX + (2*ADDR_MAX)) {
-					switch ((findex)/ADDR_MAX) {
-						case 0 : 
-							dest[findex++] = c;
-							break;
-						case 1 : 
-							src[((findex++)%ADDR_MAX)] = c;
-							break;
-						default : 
-							dgpt[((findex++)-(ADDR_MAX*2))] = c;
-							break;
-					}
-					mid = 0; //make sure there wasn't a random CTLFLAG byte
-				}
-			}
-			else if (info == 1 && findex < DATA_MAX) {
-				if (c == FLAG) {
-					print_packet(dest,src,dgpt,data);
-					recv = 0;
-					break;
-				}
-				data[findex] = c;
-			}
-			else
-				error_handle("[!!] Error during listen.\n");
-		}
-		else if (c == FLAG) {
-			recv = 1;
-		}
-		throughput++;
-	}
-	return 0;
+	return recieve();
 }
 
 void error_handle (char * error_string)
@@ -96,3 +44,69 @@ void print_packet (char * dest, char * src, char * dgpt, char * data)
 	printf("%s,%s,%s,%s\n",dest,src,dgpt,data);
 }
 
+int recieve (void)
+{
+	char dest[ADDR_MAX];
+	char src[ADDR_MAX];
+	char dgpt[DGPT_MAX];
+	char data[DATA_MAX];
+
+	int c,findex,i,throughput;
+	int recv,addr,mid,info;
+
+	findex = throughput = 0;
+	addr = 1;
+	recv = mid = info = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (1 == recv) {
+			if (1 == addr) {
+				if (c == CTLFLAG) {
+					mid = 1;
+				}
+				else if (c == PIDFLAG && mid == 1) {
+					addr = 0;
+					info = 1;
+					findex = 0;
+				}
+				else if (findex < DGPT_MAX + (2*ADDR_MAX)) {
+					switch ((findex)/ADDR_MAX) {
+						case 0 : 
+							dest[findex++] = c;
+							break;
+						case 1 : 
+							src[((findex++)%ADDR_MAX)] = c;
+							break;
+						default :
+							dgpt[((findex++)-(ADDR_MAX*2))] = c;
+							break;
+					}
+					mid = 0; //make sure there wasn't a random CTLFLAG byte
+				}
+				else {
+					//Reached maximum addr field index
+					addr = 0;
+					findex = 0;
+					info = 1;
+				}
+			}
+			else if (1 == info && findex < DATA_MAX) {
+				if (c == FLAG) {
+					print_packet(dest,src,dgpt,data);
+					recv = 0;
+					break;
+				}
+				data[findex++] = c;
+			}
+			else {
+				recv = 0; //reached packet size limit, not a packet. RST.
+				error_handle("[!!] Error during listen.\n");
+			}
+		}
+		else if (c == FLAG) {
+			recv = 1;
+		}
+		throughput++;
+	}
+	return throughput;
+}
